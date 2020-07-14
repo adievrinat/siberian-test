750e8415d537201da32d1be7b7d803b7e11c3344
Ринат <adievrinat@yandex.ru>
2020-07-14 20:40:11 +0700

    info обработчик на resize

--- src/blocks/info/info.js
+++ src/blocks/info/info.js
@@ -1,13 +1,31 @@
 class Info {
   constructor() {
+    if (typeof Info.instance === "object") {
+      return Info.instance;
+    }
+
     this.component = document.querySelector("[data-info]");
     this.title = this.component.querySelector("[data-title]");
     this.loadingCls = "info_loading";
 
-    if (window.innerWidth > 767) {
-      !this.component.classList.contains(this.loadingCls) ? this.component.classList.add(this.loadingCls) : '';
-      this.typingText(this.title);
-    }
+    this.animation = () => {
+      if (window.innerWidth > 767) {
+        !this.component.classList.contains(this.loadingCls) ? this.component.classList.add(this.loadingCls) : '';
+        this.typingText(this.title);
+      }
+    };
+
+    this.animation();
+    this.events();
+
+    Info.instance = this;
+    return this;
+  }
+
+  events() {
+    window.addEventListener("resize", () => {
+      this.animation();
+    });
   }
 
   typingText(element) {
